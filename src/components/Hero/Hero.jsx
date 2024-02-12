cccac3038825f3c9d27e7382e3e4313fa6759c3c
TuoxinLi <txli299@gmail.com>
2024-02-11 21:56:05 -0500

    Personal site implemented

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Cliff</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 2 years of experience using multiple technologies. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:li.tuox@northeastern.edu" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
